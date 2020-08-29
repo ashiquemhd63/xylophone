d44f9ccaa9324444c4ded81c38eb0483a344cf35
Muhammed Ashique <ashiquemhd1122@gmail.com>
2020-08-29 09:42:36 +0530

    just played an audio player

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
-
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -10,7 +9,14 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Text(nouns.first),
+          child:Center(
+            child: FlatButton(onPressed: (){
+              final player =AudioCache();
+              player.play('note1.wav');
+            },
+                child: Text("Click me"),
+            ),
+          ) ,
         ),
       ),
     );
