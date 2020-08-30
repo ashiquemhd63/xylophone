04947e364fef354e22cd9d1953e449d1070d85b0
Muhammed Ashique <ashiquemhd1122@gmail.com>
2020-08-30 12:19:34 +0530

    project completed and all songs are added and customised the app

--- lib/main.dart
+++ lib/main.dart
@@ -4,21 +4,47 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  //int songNumber;
+  void playSound(int songNumber){
+    final player =AudioCache();
+    player.play('note$songNumber.wav');
+  }
+  Expanded buildKey(Color color ,int num){
+    return Expanded(
+      child: FlatButton(
+
+
+        color: color,
+        onPressed: (){
+          playSound(num);
+        },
+
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child:Center(
-            child: FlatButton(onPressed: (){
-              final player =AudioCache();
-              player.play('note1.wav');
-            },
-                child: Text("Click me"),
-            ),
+          child:Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+
+            buildKey(Colors.red,1),
+              buildKey(Colors.greenAccent,2),
+              buildKey(Colors.blue,3),
+              buildKey(Colors.pink,4),
+              buildKey(Colors.yellow,5),
+              buildKey(Colors.green,6),
+              buildKey(Colors.orange,7),
+            ],
+
+          ),
           ) ,
         ),
-      ),
-    );
+      );
+
   }
 }
